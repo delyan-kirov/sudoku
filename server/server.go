fe955658012249dd04636ed700e7a0be4c6cbe3a
delyan-kirov <delyan.k.kirov@gmail.com>
2024-01-11 20:37:07 +0200

    Adding a the web application

---
+++ server/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    // "net/http"
+)
+
+func main() {
+    r := gin.Default()
+
+    // Serve static files from the build directory
+    r.Static("/static", "./build/static")
+
+    // Serve the React app's index.html
+    r.GET("/", func(c *gin.Context) {
+        c.File("./build/index.html")
+    })
+
+    port := ":8080"
+    r.Run(port)
+}
